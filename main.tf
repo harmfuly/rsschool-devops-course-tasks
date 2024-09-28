23b7e4eabe9dd920f347cd7b6ef74548b4009cbc
harmfuly <harmfuly7@gmail.com>
2024-09-28 23:53:08 +0300

    feat: Create an IAM role for Github Actions

--- main.tf
+++ main.tf
@@ -8,9 +8,57 @@ terraform {
 
   backend "s3" {
     bucket         	   = "rss-task-bucket"
-    key              	   = "state/terraform.tfstate"
+    key                = "state/terraform.tfstate"
     region         	   = "eu-north-1"
     encrypt        	   = true
     dynamodb_table = "rss-table"
   }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_iam_role" "GithubActionsRole" {
+  name               = "GithubActionsRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com" 
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "GithubActionsPolicy" {
+  name        = "GithubActionsPolicy"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:*",
+          "route53:*",
+          "s3:*",
+          "iam:*",
+          "vpc:*",
+          "sqs:*",
+          "events:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = aws_iam_policy.GithubActionsPolicy.arn
 }
