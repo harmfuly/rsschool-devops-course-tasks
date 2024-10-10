e7032ccea1e51c7fa060d76048179a3fd1d007b9
harmfuly <harmfuly7@gmail.com>
2024-10-10 23:43:33 +0300

    fix: bugs with running

--- iam.tf
+++ iam.tf
@@ -5,13 +5,13 @@ provider "aws" {
 
 resource "aws_iam_openid_connect_provider" "github_oidc" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["74F3A68F16524F15424927704C9506F55A9316BD"] 
+  thumbprint_list = ["74F3A68F16524F15424927704C9506F55A9316BD"]
   url             = "https://token.actions.githubusercontent.com"
 }
 
 resource "aws_iam_role" "GithubActionsRole" {
   name = "GithubActionsRole"
-  
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
