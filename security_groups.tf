b5753658ce662cfd9985e9d778ef6f9540fd1b73
harmfuly <harmfuly7@gmail.com>
2024-10-13 19:50:31 +0300

    fix: change data in subnets.tf

--- security_groups.tf
+++ security_groups.tf
@@ -2,13 +2,13 @@ resource "aws_security_group" "inter_subnet_sg" {
   vpc_id = aws_vpc.my_vpc.id
   ingress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = [aws_vpc.my_vpc.cidr_block]
   }
   egress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
