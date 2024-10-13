8d6545a19da85935d60720a728c2826fbbaadf0e
harmfuly <harmfuly7@gmail.com>
2024-10-14 00:42:38 +0300

    feat: changed intances.tf

--- instances.tf
+++ instances.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-north-1a"
 }
 
