223fd9a4ac503c707997487b6979009cbc5b1eab
harmfuly <harmfuly7@gmail.com>
2024-10-13 22:34:17 +0300

    fix: fixed check

--- subnets.tf
+++ subnets.tf
@@ -6,10 +6,10 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "public_subnet_1" {
-  provider          = aws.main
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.public_subnet_cidrs[0]
-  availability_zone = var.azs[0]
+  provider                = aws.main
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidrs[0]
+  availability_zone       = var.azs[0]
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet 1"
@@ -17,10 +17,10 @@ resource "aws_subnet" "public_subnet_1" {
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  provider          = aws.main
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.public_subnet_cidrs[1]
-  availability_zone = var.azs[1]
+  provider                = aws.main
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidrs[1]
+  availability_zone       = var.azs[1]
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet 2"
