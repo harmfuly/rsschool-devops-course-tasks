bdd444c9e3cffd5c0efb12b8cc3ca6589b55fddd
harmfuly <harmfuly7@gmail.com>
2024-10-13 19:31:55 +0300

    feat: created security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "inter_subnet_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.my_vpc.cidr_block]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Inter-Subnet Security Group"
+  }
+}
