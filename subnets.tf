07139bdc78f409f8f5596d752b4a3a26bee6152f
harmfuly <harmfuly7@gmail.com>
2024-10-14 22:17:55 +0300

    feat: add private subnets

--- subnets.tf
+++ subnets.tf
@@ -15,3 +15,23 @@ resource "aws_subnet" "public_subnet_2" {
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1b"
 }
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Private Subnet 1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Private Subnet 2"
+  }
+}
